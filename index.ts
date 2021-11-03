c2fb03976c04b59e787344186ff05d888a340200
Chris <chur6798@stu.raritanval.edu>
2021-11-03 15:00:26 -0400

    Do changes to files

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import DiscordJS, { Intents } from 'discord.js';
 import dotenv from 'dotenv';
+import cowsay from 'cowsay';
 dotenv.config();
 
 const client = new DiscordJS.Client({
@@ -7,15 +8,26 @@ const client = new DiscordJS.Client({
 });
 
 client.on('ready', () => {
-console.log('The bot is ready')
+  console.log('The bot is ready');
 });
 
 client.on('messageCreate', (message) => {
-if (message.content === 'ping'){
-message.reply({
-content: 'pong',    
-})
-}
-})
+  if (message.content === 'ping') {
+    message.reply({
+      content: 'pong',
+    });
+  }
+  if (message.content === 'cowsay') {
+    let output: string = cowsay.say({ text: 'Hello from typescript!' });
+    console.log(output);
+    message.reply({
+      content: `
+      \`\`\`
+      ${output}
+      \`\`\`
+      `,
+    });
+  }
+});
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
