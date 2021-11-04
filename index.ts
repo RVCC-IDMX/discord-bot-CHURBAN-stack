bb5c5b5845c8272c5142ca1a77bfe073d9a01728
Christopher hurban <54687117+CHURBAN-stack@users.noreply.github.com>
2021-11-03 20:36:33 -0400

    Do update of index

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import DiscordJS, { Intents } from 'discord.js';
 import dotenv from 'dotenv';
+import cowsay from 'cowsay';
 dotenv.config();
 
 const client = new DiscordJS.Client({
@@ -7,15 +8,45 @@ const client = new DiscordJS.Client({
 });
 
 client.on('ready', () => {
-console.log('The bot is ready')
+  console.log('The bot is ready');
 });
 
 client.on('messageCreate', (message) => {
-if (message.content === 'ping'){
-message.reply({
-content: 'pong',    
-})
-}
-})
 
-client.login(process.env.TOKEN)
+  /*  ping */
+  if (message.content === 'ping') {
+    message
+      .react('🤔')
+      .then(() => console.log(`Reacted to message "${message.content}"`))
+      .catch(console.error);
+    message
+      .reply({
+        content: 'pong',
+      })
+      .then(() => console.log(`Replied to message "${message.content}"`))
+      .catch(console.error);
+  }
+
+  /* cowsay */
+  if (message.content === 'cowsay') {
+    message
+      .react('')
+      .then(() => console.log(`Reacted to message "${message.content}"`))
+      .catch(console.error);
+      let output: string = cowsay.say({ text: 'Hello from typescript!' });
+    console.log(output);
+    message
+      .reply({
+        content: `
+        \`\`\`
+        ${output}
+        \`\`\`
+        `,
+      })
+      .then() => console.log(`Replied to message "${message.content}"`)
+      .catch(console.error);
+    }
+  });
+
+client.login(process.env.TOKEN);
+
