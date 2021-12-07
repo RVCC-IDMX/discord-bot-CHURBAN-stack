050bc427bafda12218dd238eafa20136e28b9005
Chris <chur6798@stu.raritanval.edu>
2021-12-07 16:22:03 -0500

    Add add.ts file

---
+++ src/add.ts
@@ -0,0 +1,16 @@
+import { Message } from 'discord.js';
+
+//JS
+// module.exports - {}
+
+//TS
+export default {
+  callback: (message: Message, ...args: string[]) => {
+    let sum = 0;
+
+    for (const arg of args) {
+      sum += parseInt(arg);
+    }
+    message.reply('The sum is ${sum}');
+  },
+};
