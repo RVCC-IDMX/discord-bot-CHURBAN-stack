06b5d51ec52f4b92699de3ad065b0022b662360d
Chris <chur6798@stu.raritanval.edu>
2021-12-07 16:51:36 -0500

    Add multiply ts file

---
+++ src/commands/multiply.ts
@@ -0,0 +1,16 @@
+import { Message } from 'discord.js';
+
+//JS
+// module.exports - {}
+
+//TS
+export default {
+  callback: (message: Message, ...args: string[]) => {
+    let sum = 1;
+
+    for (const arg of args) {
+      sum *= parseInt(arg);
+    }
+    message.reply(`The sum is ${sum}`);
+  },
+};
